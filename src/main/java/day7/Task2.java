318fa2a088e832bc3aaa377c70b7929eacc467e4
Админ <goshlolka@gmail.com>
2022-12-03 12:22:53 +0300

    changes made from PC

---
+++ src/main/java/day7/Task2.java
@@ -0,0 +1,36 @@
+package day7;
+
+
+
+public class Task2 {
+    public static void main(String[] args) {
+
+
+        Player player1 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player2 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player3 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player4 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player5 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player6 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player7 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player8 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player9 = new Player((int)(Math.random() * (100 - 90)) + 90);
+        Player player10 = new Player((int)(Math.random() * (100 - 90)) + 90);
+
+
+        while (player1.getStamina() >= 0){
+            player1.run();
+        }
+
+        int b = player1.getStamina();
+        System.out.println(b);
+
+        int z = player1.getCountPlayers();
+        System.out.println(z);
+
+    }
+}
+
+// подумать как не допустить отрицательное значение стамины
+// подумать как правильно декрементаировать значение количества игроков через метод (сейчас почему то не отрадатывает)
+
