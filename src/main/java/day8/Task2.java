ac010aced0d6cd526fe7ea8dc077badf6f4c0132
georgeakhmedov <goshlolka@gmail.com>
2022-12-04 22:00:34 +0300

    Day 9 homework halfDONE!

--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -3,5 +3,10 @@ package day8;
 public class Task2 {
     public static void main(String[] args) {
 
+
+
+
+
+
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -3,5 +3,6 @@ package day9.Task2;
 public class TestFigures {
     public static void main(String[] args) {
 
+
     }
 }
